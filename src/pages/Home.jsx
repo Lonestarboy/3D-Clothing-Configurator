fcfdd6a4498a193c3f2fcad2ca6355c011f1932c
Lonestarboy <117044701+Lonestarboy@users.noreply.github.com>
2025-06-22 12:02:58 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
             <img 
               src='./threejs.png'
               alt="logo"
-              className="w-8 h-8 object-contain"
+              className="w-12 h-12 object-contain"
             />
           </motion.header>
 
